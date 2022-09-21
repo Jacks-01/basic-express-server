1426ff266017e01022d58b3de1fe8bf87143b6a5
Jack Stubblefield <jack011601@gmail.com>
2022-09-21 15:06:31 +0000

    person route is working correctly

--- src/server.js
+++ src/server.js
@@ -3,7 +3,21 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
+
+app.get('/person', (req, res, next) => {
+
+  let { name } = req.query;
+  try {
+    if (name) {
+      res.status(200).json(`name: ${name}`);
+    } else { res.status(500).send('SERVER ERROR'); }
+
+  } catch (err) {
+    // console.error(err);
+    next(err.message);
+  }
+});
 
 function start() {
   app.listen(PORT, () => { console.log(`you are listening on port ${PORT}`); });
